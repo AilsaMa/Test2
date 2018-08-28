a923bd4ece6cdb0e3c0ccd36f6dca8682450de0c
majun <majun@baba88.com>
2018-08-28 10:58:22 +0800

    new package

--- src/main/java/com/mj/springcloudserver/controller/TestController.java
+++ src/main/java/com/mj/springcloudserver/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.mj.springcloudserver;
+package com.mj.springcloudserver.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
